8e9a96cde175c436a3e929c9a7be596a45cbf0a5
Sergey Stadnikov <serresstad1231@gmail.com>
2026-04-28 17:23:47 +0200

    Update extraccion.py

--- aws_github/extraccion.py
+++ aws_github/extraccion.py
@@ -4,19 +4,6 @@ from curl_cffi import requests
 import pandas as pd
 import boto3 # Necesario para conectarse a S3
 
-# --- FUNCIÓN DE SUBIR A S3 ---
-def subir_a_s3(archivo_local, bucket, carpeta):
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
-    )
-    ruta_s3 = f"{carpeta}/{archivo_local}"
-    try:
-        s3.upload_file(archivo_local, bucket, ruta_s3)
-        print(f"☁️ ¡Subido a S3 con éxito!: s3://{bucket}/{ruta_s3}")
-    except Exception as e:
-        print(f"❌ Error al subir a S3: {e}")
 
 # --- FUNCIÓN DE EXTRACCIÓN (API) ---
 def extraer_datos_pccom_api():
