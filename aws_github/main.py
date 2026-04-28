2a64d6eee48107feed7c9b4931c755614d0d7729
Sergey Stadnikov <serresstad1231@gmail.com>
2026-04-28 13:08:02 +0200

    Update main.py

--- aws_github/main.py
+++ aws_github/main.py
@@ -3,12 +3,13 @@ import pandas as pd
 from extraccion import extraer_datos_pccom
 from subir_s3 import subir_a_s3
 import os
+from extraccion import url_objetivo
 
 if __name__ == "__main__":
     print("🚀 Iniciando el proceso completo...")
     
     # 1. Extraer
-    lista_productos = extraer_datos_pccom()
+    lista_productos = extraer_datos_pccom(url_objetivo)
     
     if lista_productos:
         # 2. Procesar a DataFrame
